d6cc9a2697eddf6febaf94eb4285d5af69cfe9d1
Michael Miller <41575314+Miller202@users.noreply.github.com>
2019-11-30 17:11:09 -0300

    Update merge_sort.c

--- Sort/merge_sort.c
+++ Sort/merge_sort.c
@@ -57,14 +57,14 @@ int main()
 	int a[n];
 
 	for (i = 0; i < n; i++)
-    {
+    	{
         scanf("%d", &a[i]);
 	}
 
 	merge_sort(a , 0, n-1);
 
 	for (i = 0; i < n; i++)
-    {
+    	{
         printf("%d\n", a[i]);
 	}
 
